b503594a51f475d5b5755a3873912cf5fe029ce3
Carlos Vargas <carlosvq1337@gmail.com>
2022-08-05 19:52:25 -0600

    added description to ant product

--- pages/productos/hormigas.js
+++ pages/productos/hormigas.js
@@ -4,7 +4,7 @@ import Producto from '../../components/producto'
 const productPage = () => {
     return (
         <PageLayout title="Hormigas">
-            <Producto name="Hormigas" status="Disponible" imgSrc="/products/amigurumis/hormingas.jpg" waMsg="Hola, me interesan las hormigas ofrecidas en Lovely Things!" size="22 x 6 cm"/>
+            <Producto name="Hormigas" status="Disponible" imgSrc="/products/amigurumis/hormingas.jpg" waMsg="Hola, me interesan las hormigas ofrecidas en Lovely Things!" size="22 x 6 cm" description="Hormigas con floreritos miniatura y extremidades plegables para una amplia variedad de poses"/>
         </PageLayout>
     )
 }
